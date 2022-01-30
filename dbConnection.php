e73a1bd193f810e8e3d2390ea8973edc798f1ba5
02shubhamranjan <78600429+02shubhamranjan@users.noreply.github.com>
2022-01-30 20:55:40 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,11 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$port = $SERVER['WEBSITE_MYSQL_PORT'];
-$server = "localhost:$port";
-$user = "azure";
-$password = "6#vWHD_$";
-$db = "localdb";
-
-$con= new mysqli($server, $user, $password, $db)or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('sql671.main-hosting.eu','u429173902_exam','Exam@123','u429173902_exam')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
